2dfcb0ab4db91d4c58f32609537b931f5e535fa9
George  Pianka <gpianka1@gmail.com>
2018-04-20 10:57:44 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,5 +15,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.match(//) ? true : false
+  phone.match(/\A([+]\d{1,2}\s)?[(]?\d{3}[)]?[\s_.-]?\d{3}[\s_.-]?\d{4}\z/) ? true : false
 end
